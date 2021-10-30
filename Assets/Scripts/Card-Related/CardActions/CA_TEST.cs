54558508e2befc6dda12a8d8c1152bb7ce94c345
Tyler Gentry <tygentry00@gmail.com>
2021-10-30 14:18:47 -0400

    Beginning CardAction implementation

--- Assets/Scripts/Card-Related/CardActions/CA_TEST.cs
+++ Assets/Scripts/Card-Related/CardActions/CA_TEST.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class CA_TEST : CardActionTemplate
 {
-    public override void OnPlay()
+    public override void OnPlay(Card c)
     {
-        base.OnPlay();
+        base.OnPlay(c);
         print("CA_TEST ON PLAY");
         //do other things
     }
