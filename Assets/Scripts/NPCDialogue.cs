50dd2bb74af57afe0b0c2c839c5c1bfd08272d85
dantk2001 <keegan.dant2143@gmail.com>
2021-11-02 18:33:17 -0400

    Have somewhat of dialogue system working
    Single line of dialogue that is just opening statement

--- Assets/Scripts/NPCDialogue.cs
+++ Assets/Scripts/NPCDialogue.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPCDialogue : MonoBehaviour
 {
-    //public Text uitext;
+    public Text uitext;
     public string opening;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        uitext.text = "";
     }
 
     // Update is called once per frame
@@ -24,7 +25,16 @@ public class NPCDialogue : MonoBehaviour
         //other.name should equal the root of your Player object
         if (other.name == "Player")
         {
+            uitext.text = opening;
+        }
+    }
 
+    void OnTriggerExit(Collider other)
+    {
+        //other.name should equal the root of your Player object
+        if (other.name == "Player")
+        {
+            uitext.text = "";
         }
     }
 }
