c524fa7e6f87620c9c85719e53f54e8b1fd30acf
dantk2001 <keegan.dant2143@gmail.com>
2021-12-01 15:26:41 -0500

    Fixed small camera change issue in tutorial

--- Assets/Scripts/TitleCamera.cs
+++ Assets/Scripts/TitleCamera.cs
@@ -7,24 +7,26 @@ public class TitleCamera : MonoBehaviour
     public GameObject mainCamera;
     public GameObject topCamera;
     public GameObject player;
+    bool completed = false;
 
     IEnumerator waiter()
     {
         yield return new WaitForSeconds(4);
         mainCamera.SetActive(true);
         topCamera.SetActive(false);
+        completed = true;
         player.GetComponent<OverworldMovement>().SetCanMove(true);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         //check if player is the collision
-        if (other.gameObject.name == "Player")
+        if (other.gameObject.name == "Player" && !completed)
         {
             mainCamera.SetActive(false);
             topCamera.SetActive(true);
             player.GetComponent<OverworldMovement>().SetCanMove(false);
-
+            Debug.Log(player.GetComponent<OverworldMovement>().canMove);
             StartCoroutine(waiter());
         }
     }
