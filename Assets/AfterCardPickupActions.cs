08c11c80cb9a5cfebe796a1e215b10b3a13c5b4b
unknown <idemaj@rpi.edu>
2021-12-03 21:32:30 -0500

    logic for after anim

---
+++ Assets/AfterCardPickupActions.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AfterCardPickupActions : AfterCutsceneActions
+{
+    public override void TakeActionsAfterCutscene()
+    {
+        Debug.LogError("MAKE CHANGES AND SET VARIABLES AFTER CARD CUTSCENE");
+        AfterCutsceneActions.HasCards = true;
+    }
+}
--- Assets/Animation/AfterCutsceneActions.cs
+++ Assets/Animation/AfterCutsceneActions.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AfterCutsceneActions : MonoBehaviour
 {
+    public static bool HasCards = false;
+
     // BASE CLASS TO PERFORM AFTER CUTSCENE ACTIONS
     virtual public void TakeActionsAfterCutscene()
     {
