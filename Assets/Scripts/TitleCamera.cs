2c495afb66b6f9d830d0743d1fb112bfef72741c
dantk2001 <keegan.dant2143@gmail.com>
2021-12-04 00:30:22 -0500

    working on camera change animation
    broke rn

--- Assets/Scripts/TitleCamera.cs
+++ Assets/Scripts/TitleCamera.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class TitleCamera : MonoBehaviour
 {
     public GameObject mainCamera;
-    public GameObject topCamera;
+    //public GameObject topCamera;
+    public Animation anim;
     public GameObject player;
     bool completed = false;
 
     IEnumerator waiter()
     {
         yield return new WaitForSeconds(4);
-        mainCamera.SetActive(true);
-        topCamera.SetActive(false);
+        //mainCamera.SetActive(true);
+        //topCamera.SetActive(false);
         completed = true;
         player.GetComponent<OverworldMovement>().SetCanMove(true);
     }
@@ -23,8 +24,10 @@ public class TitleCamera : MonoBehaviour
         //check if player is the collision
         if (other.gameObject.name == "Player" && !completed)
         {
-            mainCamera.SetActive(false);
-            topCamera.SetActive(true);
+            //mainCamera.SetActive(false);
+            //topCamera.SetActive(true);
+            mainCamera.transform.position = new Vector3(-424.5829162597656f, 8.026437759399414f, 397.9913635253906f);
+            anim.Play();
             player.GetComponent<OverworldMovement>().SetCanMove(false);
             StartCoroutine(waiter());
         }
