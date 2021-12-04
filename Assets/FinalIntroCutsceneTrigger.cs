85c74318a386551697e62af883a9609ef573b789
unknown <idemaj@rpi.edu>
2021-12-04 11:41:31 -0500

    final animation

--- Assets/FinalIntroCutsceneTrigger.cs
+++ Assets/FinalIntroCutsceneTrigger.cs
@@ -8,6 +8,7 @@ public class FinalIntroCutsceneTrigger : CutsceneTrigger
     public GameObject warrior;
     public GameObject mechanist;
 
+
     // Start is called before the first frame update
     void Start()
     {
