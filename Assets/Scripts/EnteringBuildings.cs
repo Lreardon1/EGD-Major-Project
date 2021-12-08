ff4f7e263bc963abb17f0027de1620045d1bc725
unknown <idemaj@rpi.edu>
2021-12-08 11:57:06 -0500

    final dungeon fixes

--- Assets/Scripts/EnteringBuildings.cs
+++ Assets/Scripts/EnteringBuildings.cs
@@ -8,11 +8,16 @@ public class EnteringBuildings : MonoBehaviour
     public int sceneToLoad;
     public Transform savedLoc;
 
+    public bool requiresWholeParty = false;
+
     void OnTriggerEnter(Collider other)
     {
         //other.name should equal the root of your Player object
         if (other.name == "Player")
         {
+            if (requiresWholeParty && (PlayerPrefs.GetInt("warrior", 0) == 0 || PlayerPrefs.GetInt("hunter", 0) == 0))
+                return;
+
             MemorySceneLoader.SetPlayerPosPref(savedLoc.position);
 
             //The scene number to load (in File->Build Settings)
